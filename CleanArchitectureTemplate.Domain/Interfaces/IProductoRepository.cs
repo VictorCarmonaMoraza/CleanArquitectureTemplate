3c815cc4113996c60d78665b34c88a07765a87f4
Victor Carmona <VictorCarmona9035@hotmail.com>
2026-09-22 09:43:33 +0200

    Agregar archivos de proyecto.

---
+++ CleanArchitectureTemplate.Domain/Interfaces/IProductoRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitectureTemplate.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureTemplate.Domain.Interfaces
+{
+    public interface IProductoRepository
+    {
+        Task<IEnumerable<Producto>> ObtenerTodosAsync();
+        Task<Producto> ObtenerPorIdAsync(int id);
+        Task<Producto> ObtenerPorNombreAsync(string nombre);
+        Task AgregarAsync(Producto producto);
+        Task ActualizarAsync(Producto producto);
+        Task EliminarAsync(int id);
+    }
+}
---
+++ CleanArchitectureTemplate.Infraestructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitectureTemplate.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitectureTemplate.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //Agregamos las entidades o modelos con DbSet de nuestra aplicación aquíÇ
+        public DbSet<Producto> Productos { get; set; }
+
+    }
+}
