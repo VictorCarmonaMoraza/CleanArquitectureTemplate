4399df0b575add67ed8b095c8ce0213eb0d8a441
Victor Carmona <VictorCarmona9035@hotmail.com>
2026-09-22 09:54:48 +0200

    DTOs (Data Transfer Object)

---
+++ CleanArchitectureTemplate.Application/DTOs/ProductoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchitectureTemplate.Application.DTOs
+{
+    public class ProductoDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre del producto es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre del producto no puede exceder los 100 caracteres")]
+        public string Nombre { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio del producto debe ser mayor que cero")]
+        public decimal Precio { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
--- CleanArchitectureTemplate.Domain/Interfaces/IProductoRepository.cs
+++ CleanArchitectureTemplate.Domain/Interfaces/IProductoRepository.cs
@@ -1,9 +1,4 @@
 ﻿using CleanArchitectureTemplate.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchitectureTemplate.Domain.Interfaces
 {
